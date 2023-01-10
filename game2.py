0147d6f5690138d9630a72ed8d970e9b24094d2e
JayroGG <jaxjayro@gmail.com>
2023-01-10 03:52:06 -0600

    Added battle menu and mechanism

--- game2.py
+++ game2.py
@@ -1,20 +1,26 @@
+import os
+from time import sleep
+
 #Class Hero
 class Hero:
   health = 100
   #Constructor
-  def __init__(self, damage, target):
-    self.damage = damage
-    self.target = target
+  def __init__(self, damage):
+    self.damage = damage    
 
   #Methods
   def attack(self):
     self.target.get_damage(self.damage)
     print(f'you have attacked the monster, the monster has: {self.target.health} HP')
+    #Verifies if the target is still alive or if it have enough energy to attack
     if self.target.health > 0:
       if self.target.energy >= 20:
         self.target.autoAttack(self)
     else:
-      print('The Monster is dead')
+      print('The Monster is dead you have won')
+
+  def setTarget(self, target):
+    self.target = target
     
 
 #Class Monster
@@ -30,7 +36,7 @@ class Monster:
   
   #Dunder Methods
   def __call__(self):
-    return 'The Monster has been summoned'
+    return '\nThe Monster has been summoned'
   
   def __str__(self):
     return 'A Monster'
@@ -44,8 +50,14 @@ class Monster:
     self.energy -= 20        
     print(f'The Monster has attacked you in response, now you have: {target.health} HP')  
 
-  def move(self):
+  def move(self, speed):
     print('The Monster has moved')
+    print(f'it has a speed of {speed}')
+  
+  def attributes(self):
+    print(f'Monster damage: {self.auto_attack}')
+    print(f'Monster health: {self.health}')
+    print(f'Monster energy: {self.energy}')
 
 
 #Creating Shark lass with ineherence of the Monster class
@@ -59,20 +71,60 @@ class Shark(Monster):
   #Methods
   def bite(self, target):
     target.health -= 35
+  
+  def move(self):
+    print('The shark has moved')
+    print(f'The speed of the shark is {self.speed}')
 
+#Creating a Scorpion Monster class
+class Scorpion(Monster):
+  #Dunder methods
+  def __init__(self, auto_attack, health, energy):
+    super().__init__(auto_attack, health, energy)
+    self.poison_damage = auto_attack
+
+  #Methods
+  def autoAttack(self, target):
+    print(f'The Scorpion has attacked in response \n It has deal: {self.poison_damage} of poison damage')
+    target.health -= self.poison_damage
+    print(f'Now you have: {target.health} HP')
 
 #Creating a monsters objects
 shark = Shark(120, 30, 100, 100)
 monster = Monster(15, 100, 100)
+scorpion = Scorpion(25, 130, 100)
 
 #Creating hero object with damage and target monster
-hero = Hero(damage = 50, target = shark)
-print(shark())
-print(shark)
+hero = Hero(damage = 50)
 
-#Executing attack
-hero.attack()
-hero.attack()
+#Creating a battle
+def battle(hero, monster):
+  print(monster())
+  print(monster)
+  hero.setTarget(monster)
+  
+  while monster.health > 0:
+    print(f'\nHero health: {hero.health}') 
+    if hero.health <= 0:
+      print('Your HP have lowered to critic, you are diying, heal yourself and try again')
+      return
 
-print(shark.health)
-print(monster.health)
+    monster.attributes()
+    print('\n1. Attack\n2.Defend\n3.Do nothing\n4.Escape')
+    action = input('\nWhat you gonna do?\n')
+    if action == '1':
+      hero.attack()
+    if action == '2':
+      print('\nThe monster has attack but has no effect on you') 
+    if action == '3':
+      monster.autoAttack(hero)
+    if action == '4':
+      if hero.damage > monster.auto_attack:
+        print('\n\nYou have escpaped\n\n')
+        return 
+      else:
+        print("\nYou can't escape\n")  
+
+  print('\n\nThe battle has ended\n\n')    
+
+battle(hero, scorpion)
